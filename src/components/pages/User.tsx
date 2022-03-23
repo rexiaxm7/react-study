7a4d0a6c1ec0af86766c5ddea1f01a4ba34faf13
HIDAKAHIROMU08 <hidaka_hiromu@crossware.co.jp>
2022-03-23 20:55:20 +0900

    CRUD機能途中段階

--- src/components/pages/User.tsx
+++ src/components/pages/User.tsx
@@ -1,48 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import "./User.css";
 import { DataGrid } from '@mui/x-data-grid';
-
-const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'user_name',
-      headerName: 'ユーザ―名',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'mail',
-      headerName: 'メールアドレス',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: '年齢',
-      type: 'number',
-      width: 100,
-      editable: true,
-    },
-    {
-        field: 'job',
-        headerName: '職業',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'gender',
-        headerName: '性別',
-        width: 100,
-        editable: true,
-    },
-    {
-        field: 'interests',
-        headerName: '趣味',
-        width: 200,
-        editable: true,
-    },
-  ];
-  
+import Button from '@mui/material/Button';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -59,18 +19,86 @@ export default () => {
     };
 
     fetchData();
-}, []);
+    }, []);
+
+    const deleteUser = React.useCallback(
+        (id) => () => {
+          setTimeout(() => {
+            setData((prevRows) => prevRows.filter((data) => id !== id));
+          });
+        },
+        [],
+      );
+
+    const columns = [
+        { field: 'id', 
+          headerName: 'ID', 
+          width: 90 
+        },
+        {
+          field: 'mail',
+          headerName: 'メールアドレス',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'age',
+          headerName: '年齢',
+          type: 'number',
+          width: 100,
+          editable: true,
+        },
+        {
+            field: 'job',
+            headerName: '職業',
+            width: 150,
+            editable: true,
+        },
+        {
+            field: 'gender',
+            headerName: '性別',
+            width: 100,
+            editable: true,
+        },
+        {
+            field: 'interests',
+            headerName: '趣味',
+            width: 200,
+            editable: true,
+        },
+        {
+            field: 'actions',
+            type: 'Actions',
+            headerName: '操作',
+            width: 110,
+        }
+    ];
+
+
+function editToolBar() {
+    const handleClick = () =>{
+    };  
 
     return (
-        <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={data}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-          //disableSelectionOnClick
-        />
-      </div>
+        <Button className="addition" variant="contained" color="primary" onClick={handleClick}>
+        + 追加
+        </Button>
     );
-  }
+};
+        
+return (
+    <div style={{ height: 400, width: '100%' }}>
+    <DataGrid
+      rows={data}
+      columns={columns}
+      pageSize={5}
+      rowsPerPageOptions={[5]}
+      components={{
+        Toolbar: editToolBar,
+      }}
+      //checkboxSelection
+      //disableSelectionOnClick
+    />
+  </div>
+);
+}
