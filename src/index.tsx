fa4d21aff3ab0766334f0046824ad4768af554ee
Shimoji Ryouma <tuuykgc6aap1hbdmrwnc@docomo.ne.jp>
2022-03-24 17:33:02 +0900

    React勉強会第一弾

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM, { render } from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Test } from "./components/pages/Test";
+import DataTable from "./components/DataGrid/DataTable";
 
 const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<DataTable />} />
         <Route path="test" element={<Test />} />
       </Routes>
     </BrowserRouter>
