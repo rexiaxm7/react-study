87a554394c0497916eb7175cbb7a810113e193d4
B4129 <B4129@oic.jp>
2022-04-10 14:05:20 +0900

    add: オートコンプリートを付与

--- src/components/models/user/form/formControl/Job.tsx
+++ src/components/models/user/form/formControl/Job.tsx
@@ -1,47 +1,35 @@
-import {FormControl, InputLabel, MenuItem, Select,} from "@mui/material";
+import {Autocomplete, FormControl, InputLabel, MenuItem, Select, TextField,} from "@mui/material";
 import {FC, useContext} from "react";
-import {UserFormContext} from "../../../../../context/UserFormContext";
+import { UserFormContext} from "../../../../../context/UserFormContext";
 
 export const Job: FC = () => {
   const { job } = useContext(UserFormContext);
-
-
   const jobOptions = [
     {
-      text: "エンジニア",
+      label: "エンジニア",
       value: "エンジニア",
     },
     {
-      text: "花屋店員",
+        label: "花屋店員",
       value: "花屋店員",
     },
     {
-      text: "プロ野球選手",
+        label: "プロ野球選手",
       value: "プロ野球選手",
     },
   ];
   return (
-      <FormControl variant="standard" >
-        <InputLabel id="age-label">職業</InputLabel>
-        <Select
-            labelId="age-label"
-            id="age-select"
-            value={job}
-            // onChange={handleChange}
-            label="Age"
-            defaultValue={jobOptions[0].value}
-
-        >
-          {jobOptions.map((option) => (
-              <MenuItem
-                  value={option.value}
-                  key={option.value}
-              >
-                {option.text}
-              </MenuItem>)
-          )}
-
-        </Select>
-      </FormControl>
+          <Autocomplete
+              disablePortal
+              options={jobOptions}
+              noOptionsText={"候補が存在しません"}
+              renderInput={(params) =>
+                  <TextField
+                      {...params}
+                      label={"職業"}
+                      value={job}
+                      variant="standard"
+                  />}
+          />
   );
 };
